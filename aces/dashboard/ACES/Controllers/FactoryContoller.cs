6d77f382d98b566a4b2f901b2d102df92516a4c4
jessetaylor2 <jessetaylor2@mail.weber.edu>
2021-06-15 01:35:24 -0600

    Added some comments and cleaned up code a bit.

--- aces/dashboard/ACES/Controllers/FactoryContoller.cs
+++ aces/dashboard/ACES/Controllers/FactoryContoller.cs
@@ -25,6 +25,7 @@ namespace ACES.Controllers
 
         #endregion
 
+        #region FactoryCreate
         [HttpPost("factory")]
         public HttpResponseMessage FactoryCreate([FromBody] PostAddWatermark data)
         {
@@ -43,7 +44,7 @@ namespace ACES.Controllers
 
             if (data.existing_watermark == "")
             {
-                watermark = Generate_Watermark(data.email, data.asn_no);
+                watermark = GenerateWatermark(data.email, data.asn_no);
             }
             else
             {
@@ -59,14 +60,14 @@ namespace ACES.Controllers
             foreach (var f in markableFiles)
             {
 
-                totalMarks += WatermarkFile(data.directory);  //Change the directory to the files retrieved.
+                totalMarks += WatermarkFile(data.directory);  //Change the directory to the files retrieved. "copiedPath + "/" + f" or something that gets the correct files each time.
 
             }
 
             var json = System.Text.Json.JsonSerializer.Serialize(new GetWatermarkedAssignment()
             {
                 watermark = watermark,
-                watermark_count = WatermarkFile(data.directory),
+                watermark_count = totalMarks,
                 zipped_directory = data.directory
             });
 
@@ -74,22 +75,29 @@ namespace ACES.Controllers
 
             return response;
         }
+        #endregion
 
+        #region GenerateFileList
         private List<string> GenerateFileList(string jsonCode)
         {
 
+            //Parse the json code provided in the string parameter
             JObject json = JObject.Parse(jsonCode);
 
+            //Get file names (the children of the "namedFiles" object in the json)
             var files = json["namedfiles"].Children();
 
             var fileList = new List<string>();
 
+            //Put the file names obtained in files variable into this list object
             fileList.AddRange(files.Select(file => file.Value<string>()));
 
             return fileList;
 
         }
+        #endregion
 
+        #region WatermarkFile
         private int WatermarkFile(string path)
         {
             int watermarks = 0;
@@ -113,25 +121,32 @@ namespace ACES.Controllers
             newFile.Close();
             return watermarks;
         }
+        #endregion
 
-        private string Generate_Watermark(string email, string asn_no)
+        #region GenerateWatermark
+        private string GenerateWatermark(string email, string asn_no)
         {
 
             RNGCryptoServiceProvider rngCsp = new RNGCryptoServiceProvider();
+ 
             byte[] salt = new byte[20];
-
+            //Fill array with random bytes to be salt value
             rngCsp.GetBytes(salt);
 
+            //Change email and asn_no to bytes
             byte[] enEmail = Encoding.UTF8.GetBytes(email);
             byte[] enAsn_no = Encoding.UTF8.GetBytes(asn_no);
 
+            //Combine salt value with encoded values for email and asn_no
             List<byte> watermarkBytes = new List<byte>();
             watermarkBytes.AddRange(enAsn_no);
             watermarkBytes.AddRange(enEmail);
             watermarkBytes.AddRange(salt);
 
+            //Hash full array to get the full watermark
             byte[] bytes = SHA256.Create().ComputeHash(watermarkBytes.ToArray());
 
+            //Put hashed value into one string
             StringBuilder fullWatermark = new StringBuilder();
             for(int i = 0; i < bytes.Length; i++)
             {
@@ -143,6 +158,7 @@ namespace ACES.Controllers
             return fullWatermark.ToString();
 
         }
+        #endregion
 
     }
 }
