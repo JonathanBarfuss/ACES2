948311f1cc08cb9462f39906e2f7148dba65f0a9
TuckerGarner <tuckergarner@mail.weber.edu>
2021-07-26 18:45:32 -0600

    Added a Watermark Guide on create and edit assignment pages
    did some formatting on edit assignment page as well

--- aces/dashboard/ACES/Controllers/AssignmentsController.cs
+++ aces/dashboard/ACES/Controllers/AssignmentsController.cs
@@ -12,6 +12,7 @@ using System.Collections.Immutable;
 using System.Drawing;
 using Microsoft.EntityFrameworkCore.Internal;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace ACES.Controllers
 {
@@ -149,6 +150,9 @@ namespace ACES.Controllers
         // GET: Assignments/Create
         public IActionResult Create(int? courseId)
         {
+            string defaultJSON = "{\"files\":[{\"name\": \"\",\"lines\": 0,\"whitespaces\": [\n\n],\"randomstring\": [\n\n]}]}"; // template for json
+
+            ViewBag.JSON = JValue.Parse(defaultJSON).ToString(Formatting.Indented);
             ViewBag.CourseId = courseId;
             return View();
         }
@@ -189,6 +193,7 @@ namespace ACES.Controllers
                 return NotFound();
             }
 
+            ViewBag.JSON = JValue.Parse(assignment.JSONCode).ToString(Formatting.Indented);
             ViewBag.CourseId = assignment.CourseId;
             ViewBag.From = from; // This helps take us back to CourseAssignments if that's where we came from
             return View(assignment);
