c948008e76452621bf8834b5d06fd6eecd92ae46
hunterhayes142857 <hunterhayes@mail.weber.edu>
2022-12-02 09:27:40 -0700

    added migrations for 2FA and Admin pages

--- aces/dashboard/ACES/Migrations/20221202154843_Add2FAAndAdmin.Designer.cs
+++ aces/dashboard/ACES/Migrations/20221202154843_Add2FAAndAdmin.Designer.cs
@@ -4,6 +4,7 @@ using ACES.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ACES.Migrations
 {
     [DbContext(typeof(ACESContext))]
-    partial class ACESContextModelSnapshot : ModelSnapshot
+    [Migration("20221202154843_Add2FAAndAdmin")]
+    partial class Add2FAAndAdmin
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ aces/dashboard/ACES/Migrations/20221202154843_Add2FAAndAdmin.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ACES.Migrations
+{
+    public partial class Add2FAAndAdmin : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "TwoFactorEnabled",
+                table: "Student",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAdmin",
+                table: "Instructor",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsApproved",
+                table: "Instructor",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "TwoFactorEnabled",
+                table: "Instructor",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TwoFactorEnabled",
+                table: "Student");
+
+            migrationBuilder.DropColumn(
+                name: "IsAdmin",
+                table: "Instructor");
+
+            migrationBuilder.DropColumn(
+                name: "IsApproved",
+                table: "Instructor");
+
+            migrationBuilder.DropColumn(
+                name: "TwoFactorEnabled",
+                table: "Instructor");
+        }
+    }
+}
